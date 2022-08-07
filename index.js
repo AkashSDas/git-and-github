c91f8be5afc531bce79206bbe47d896bd0366fa0
AkashSDas <aakashdas368@gmail.com>
2022-08-07 10:01:29 +0530

    initial my bright future

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log("parrot has left me");
 console.log("i'm not even considered for the top 10 in the hackathon");
+
+console.log("these were my past failures");
+console.log("now i'll win");
+console.log("i'll win everything i want and even more than that");
